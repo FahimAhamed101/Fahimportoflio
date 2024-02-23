81adebfae81405589be5dd63e4d0c99ba6ad4ec2
fahim <fahimahamedweb@gmail.com>
2024-02-24 01:16:47 +0600

    contact form done

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,67 @@
+
+"use client"
+
+import React from "react";
+import SectionHeading from "../../components/section-heading";
+
+import { sendEmail } from "./sendEmail";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
+
+
+    type Props = {}
+ const Contact = async (props: Props) => {
+ 
+   
+  return (
+    <div
+      id="contact"
+    
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center "
+ 
+    >
+      <SectionHeading>Contact me</SectionHeading>
+
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:example@gmail.com">
+          example@gmail.com
+        </a>{" "}
+        or through this form.
+      </p>
+
+      <form
+        className="mt-10 flex flex-col dark:text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
+    </div>
+  );
+}
+
+export default Contact
