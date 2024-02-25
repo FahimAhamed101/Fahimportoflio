d7d41c286ade2b2d5da265dbe4626cbfbbac7432
fahim <fahimahamedweb@gmail.com>
2024-02-25 12:38:08 +0600

    apos fixed

--- src/app/addproduct/productform.tsx
+++ src/app/addproduct/productform.tsx
@@ -10,8 +10,8 @@ import ImageUpload from '@components/ImageUpload'
 type Props = {}
 
 const Productform = (props: Props) => {
-    const {data:session} = useSession()
-    const id = session?.user.id
+    const {data:session,} = useSession()
+    const id = session?.user?.id
     const router = useRouter()
     const [formData,setFormData] = useState({
         title:'',
