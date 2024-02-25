89359823689986636dad3f16c197e6c313eb66bc
fahim <fahimahamedweb@gmail.com>
2024-02-25 14:13:55 +0600

    added sme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,11 @@ const config: Config = {
             "code::after": false,
           },
         },
-        background: "hsl(var(--tw-gradient-stops))",
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
       },
       colors: {
         border: "hsl(var(--border))",
