a0d9f6ec318c35e888afbd5ae7de928ddec30ecd
fahim <fahimahamedweb@gmail.com>
2025-01-12 23:44:17 +0600

    updated and changed some files

--- src/app/projects/ProjectCard.jsx
+++ src/app/projects/ProjectCard.jsx
@@ -3,20 +3,32 @@ import Link from 'next/link'
 function ProjectCard({product}) {
   return (
     <>
+     
+     <div className="flex w-full  md:justify-center items-start md:gap-6 gap-3 flex-wrap">
+
+
+      <div className="w-full grid   lg:justify-center lg:grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-8 md:mt-8">
+
+        <div className='text-center'>
+
+        <img src={product.images.split(',')[0]} alt="" className='rounded-t-lg w-full h-[400px] overflow-hidden object-scale-down max-h-[400px] ' />
     
-      <div className="block  img-box h-[250px] w-[350px] lg2:w-auto mx-20px flex justify-center items-center relative overflow-hidden rounded-3xl">
-      <img src={product.images.split(',')[0]} alt="" className='object-scale-down max-h-fit max-width: 20%;' />
-              <div className="p absolute top-[-100%] left-0 h-full w-full  text-center bg-[#b004b0b7] p-4 pt-5">
-                <h1 className="font-bold text-3xl"><Link href={`/projects/${product.id}`}>{product.title}</Link></h1>
+                 
+                      <h1 className="font-bold text-black text-3xl ">
+                  
+                  <Link href={`/projects/${product.id}`}>{product.title}</Link></h1>
                
 
  <Link className="font-bold text-black" href={`/projects/${product.id}`}>  View Project  </Link>
+</div>
+     
 
-
-              </div>
+         
        
             </div>
+      </div>
       
+    
     </>
   )
 }
