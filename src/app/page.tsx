cc72b32c0c8bd7113f0f50c7fd64099f7f1313d0
fahim <fahimahamedweb@gmail.com>
2025-05-08 16:26:17 +0600

    some changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Hero from "@components/hero";
 import About from "@components/about";
 import { getCurrentUser } from './lib/session';
-import Experience from "@components/experience";
+
 export default async function Home() {
   const user = await getCurrentUser()
   return (
@@ -12,7 +12,7 @@ export default async function Home() {
       <div className="space-y-24">
         <Hero />
        <About/>
-       <Experience/>
+     
       </div>
 
      
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,17 +2,15 @@ import { Dribbble, Github, Instagram } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-between gap-8 border-t px-4 py-16 text-muted-foreground md:flex-row md:px-8">
-      <a href={`mailto:${process.env.EMAIL}`}>
+    
+    <footer className="mx-4 flex w-auto flex-col gap-10 rounded-t-2xl border-x border-t border-neutral-300 bg-neutral-100 px-4 py-10 dark:border-neutral-700 dark:bg-neutral-900 md:px-6 lg:mx-8 lg:gap-20 lg:py-16">
+              <div className="mx-5 flex flex-col items-center justify-center px-8 text-sm">
+              <div className="mb-4 mt-4 flex gap-4">
+      
+       <a href={`mailto:${process.env.EMAIL}`} className="flex rounded-lg p-1 text-neutral-800 hover:bg-neutral-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:hover:text-neutral-300">
         <p>{process.env.EMAIL}</p>@copyright Fahim
       </a>
-      <a
-            className="flex gap-8"
-            href="https://github.com/FahimAhamed101"
-          >
-     &nbsp; DResume
-          </a>
-      <div className="flex gap-8">
+      </div>  <div className="flex gap-8">
         <a
           href="https://github.com/FahimAhamed101"
           className="flex items-center gap-2"
@@ -31,6 +29,10 @@ export default function Footer() {
           <p>find me on Instagram</p>
         </a>
       </div>
+      </div>
+     
+     
+     
     </footer>
   );
 }
