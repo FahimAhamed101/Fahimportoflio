6825ad11c39e8e9549850e4b763d670e1231772b
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2026-04-26 14:17:25 +0600

    done
    some changes added

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -65,10 +65,7 @@ export default function Hero() {
               <MapPin className="h-4 w-4" />
               <span>Based in Bangladesh</span>
             </div>
-            <div className="flex items-center gap-2">
-              <Calendar className="h-4 w-4" />
-              <span>1+ years of professional experience</span>
-            </div>
+            
           </div>
           
           {/* Action buttons */}
